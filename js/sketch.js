d65abd0feb7cdc1dd14a9d90c56bd9264a04cebc
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-11-02 09:29:35 -0600

    added jpeg images

--- js/sketch.js
+++ js/sketch.js
@@ -1,53 +1,30 @@
-var bubbles = [];
-
-var Bubble = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.speedX = 40;
-  this.speedY = 0;
-  this.acceleration = 0.10;
-
-  this.display = function() {
-
-    stroke(random(255), 0, random(255));
-    strokeWeight(4);
-    noFill();
-    ellipse(this.x, this.y, 25, 25);
+var dot = {
+  x: 50,
+  y: 50,
+  diameter: 25,
+  color: {
+    r: 10,
+    g: 100,
+    b: 255
   }
-
-  this.move = function() {
-
-    this.speedY += this.acceleration;
-
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-
-  this.bounce = function(){
-    if(this.x > width || this.x < 0) {
-      this.speedX *= -1;
-    }
-  }
-};
+}
 
 function setup() {
+  //Draw a canvas
   createCanvas(window.innerWidth, window.innerHeight);
-
-  for(var i = 0; i < 1; i ++){
-    bubbles[i] = new Bubble(random(0, width), random(0, height));
-  }
+  background(255, 0);
 }
 
 function draw() {
-  background(255);
+  dot.color.r = random(100, 255);
+  dot.color.b = random(100, 255);
 
-  for(var i = 0; i < bubbles.length; i ++) {
-    bubbles[i].display();
-    bubbles[i].bounce();
-    bubbles[i].move();
-  }
-}
+  //noStroke();
 
-function mousePressed(){
-  bubbles.push(new Bubble(mouseX, mouseY));
+  fill(dot.color.r, dot.color.g, dot.color.b);
+  ellipse(dot.x, dot.y, dot.diameter, dot.diameter);
+
+  dot.x = random(0, width);
+  //Math.random() gives us a number from 0 to 1.
+  dot.y = Math.random() * height;
 }
